f3343380325578088368ca4a0cbaed5f2e7ae6e7
Ilkin <ilkinhasanov673@gmail.com>
2020-06-22 21:28:04 +0300

    ..

--- src/pro/edu/model/OrderingLunch.java
+++ src/pro/edu/model/OrderingLunch.java
@@ -1,19 +1,34 @@
-package pro.edu.model;/**
+/**
  * Ilkin Hasanov
  * <p>
  * Copyright (c) HASANOV.
  */
-//package ;
+package pro.edu.model;
 
 import pro.edu.IBill;
 
 /**
- * IKIN HASANOV's response to Homework [x], Module [x].
- *
+ * IKIN HASANOV's response to Laboratory work 2.
  *   Task:
  *
+ *       Topic: Object-oriented programming,  JavaCollectionFramework
+ *       Choose a subject according to your number. Develop a service
+ *       using all your best in OOP and Java collections.
+ *       Six methods must be written like in the following example.
+ *                      A point for each one.
+ *       Creative approaches are welcome.
  *
- * @version 1.10 [DD] [MMM] [YYYY]
+ * EXAMPLE
+ *
+ *  A company contains two types of employees:  full-time and  part-time ones.
+ * 1. Create two appropriate classes and 5 objects of each one.
+ * 2. Calculate   the total salary of a month.
+ * 3. Find the highest salary employee.
+ * 4. Find he lowest salary employee
+ * 5.  The average salary.
+ * 6. Who get more salary, part-time or full-time employees?
+ *
+ * @version 1.10    22 Jun 2020
  * @author Ilkin Hasanov
  */
 public class OrderingLunch implements IBill {
